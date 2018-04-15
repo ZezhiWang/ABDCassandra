b0ccdbdcdad807221c25e0a4adbd38d656fdf26c
zz wang <wanganh@bc.edu>
2018-04-15 16:46:19 -0400

    added user input

--- main.go
+++ main.go
@@ -2,27 +2,46 @@ package main
 
 import(
 	"fmt"
+	"flag"
 	"github.com/gocql/gocql"
 )
 
 //	Keyspace 	= demo
 //	Table		= abd(key int, id text, ver int, val text)
-//	IP			= 172.17.0.1
+
+const addrs	= []string{"172.17.0.2", "172.17.0.3", "172.17.0.4"}
 
 var (
 	id 			int
-	addrs 		[]string
 	sessions 	map[string]*gocql.Session	
 )
 
 func main() {
-	addrs = []string{"172.17.0.2"}
-	id = 0
+	flag.IntVar(&id, "id", 0, "specify the node id")
 	sessions = make(map[string]*gocql.Session)
 	for _,addr := range addrs {
 		sessions[addr] = getSession(addr)
 	}
 	defer closeAll()
-	write("test")
-	fmt.Println(read())
+
+	done := make(chan bool)
+	go userInput(done)
+}
+
+func userInput(done chan bool) {
+	reader := bufio.NewReader(os.Stdin)
+    fmt.Println("Enter Commands: ")
+    for {
+        text,_ := reader.ReadString('\n')
+        if strings.HasPrefix(text, "write"){
+        	text = strings.Replace(text, "\n", "", -1)
+        	info := strings.SplitN(text, " ", 2)[1]
+        	write(info)
+        } else if strings.HasPrefix(text, "read") {
+        	fmt.Println(read())
+        } else {
+        	break
+        }
+    }
+    done <- true
 }
