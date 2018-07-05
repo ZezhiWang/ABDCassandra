285ded271d2752bdce942aa3b9f9acd4206b6a78
Ubuntu <wanganh@bc.edu>
2018-07-05 19:46:29 +0000

    fix typo

--- server/cassConn.go
+++ server/cassConn.go
@@ -34,8 +34,8 @@ func queryGet(key string) TagVal {
 
 // update tagval to cassandra
 func querySet(tv TagVal) {
-	arg := fmt.Sprintf("INSERT INTO abd (key,id,val,ver) values (?,?,?,?) WHERE ver < ?")
-	if err := session.Query(arg, tv.Key,tv.Tag.Id,tv.Val,tv.Tag.Ts,tv.Tag.Ts).Exec(); err != nil {
+	arg := fmt.Sprintf("INSERT INTO abd (key,id,val,ver) values (?,?,?,?) WHERE ver < %d",tv.Tag.Ts)
+	if err := session.Query(arg, tv.Key,tv.Tag.Id,tv.Val,tv.Tag.Ts).Exec(); err != nil {
 		log.Fatal(err)
 	}
 }
